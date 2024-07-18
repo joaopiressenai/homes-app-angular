b1020b11e8649259a4f4bb3e224c657971f61c79
joao@aluno.senai.br <joao@aluno.senai.br>
2024-07-18 11:05:29 -0300

    Develop url

--- src/app/imovel-detalhes/imovel-detalhes.component.ts
+++ src/app/imovel-detalhes/imovel-detalhes.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Imovel } from '../imovel';
 import { ImovelService } from '../imovel.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-imovel-detalhes',
@@ -16,10 +16,16 @@ export class ImovelDetalhesComponent {
   
   constructor(
     private imvService: ImovelService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private rt: Router
   ) {
     const id = this.route.snapshot.params["id"]
     this.imovel = this.imvService.buscarImovelPeloId(id);
+  }
+
+  abrirPaginaAluguel() {
+    const url = `/detalhes/${this.imovel?.id}/aluguel`
+    this.rt.navigate([url])
   }
   
 }
