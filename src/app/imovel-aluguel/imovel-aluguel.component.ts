b53b360f9190a4499f80e81f2712ae207a4a340a
joao@aluno.senai.br <joao@aluno.senai.br>
2024-07-18 10:26:37 -0300

    Develop buttons

---
+++ src/app/imovel-aluguel/imovel-aluguel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imovel-aluguel',
+  standalone: true,
+  imports: [],
+  templateUrl: './imovel-aluguel.component.html',
+  styleUrl: './imovel-aluguel.component.css'
+})
+export class ImovelAluguelComponent {
+
+}
